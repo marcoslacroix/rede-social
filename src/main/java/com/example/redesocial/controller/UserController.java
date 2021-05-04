4136e087b22265a54f8fda8f13629bc4e6fe58f7
marcoslacroix <marcoslacroix@gmail.com>
2021-05-03 21:26:29 -0300

    Alterado Http request para Put nos update

--- src/main/java/com/example/redesocial/controller/UserController.java
+++ src/main/java/com/example/redesocial/controller/UserController.java
@@ -23,28 +23,28 @@ public class UserController {
         return ResponseEntity.ok(userService.create(userCreateDto));
     }
 
-    @PostMapping("/inactive")
+    @PutMapping("/inactive")
     @ApiOperation(value = "Inactivate user")
     public ResponseEntity<?> inactive(@RequestParam @NotBlank String uuid) {
         userService.inactive(uuid);
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/updatePassword")
+    @PutMapping("/updatePassword")
     @ApiOperation(value = "Change password")
     public ResponseEntity<?> changePassword(@RequestParam @NotBlank String newPassword,
                                             @RequestParam @NotBlank String newPassword2,
                                             @RequestParam @NotBlank String oldPassword,
                                             @RequestParam @NotBlank String uuid) {
         userService.changePassword(newPassword, newPassword2, oldPassword, uuid);
-        return ResponseEntity.ok("Password Updated sucessfully");
+        return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/updateEmail")
+    @PutMapping("/updateEmail")
     @ApiOperation(value = "Change email")
     public ResponseEntity<?> changeEmail(@RequestParam @NotBlank String newEmail,
                                          @RequestParam @NotBlank String uuid) {
         userService.changeEmailPrincipal(newEmail, uuid);
-        return ResponseEntity.ok("Email changed sucessfully");
+        return ResponseEntity.ok().build();
     }
 }
